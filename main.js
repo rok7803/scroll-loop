ca76f69ec616309f638eca78d7b4ca5523f8ccaa
ha6icht <rolf.karlen@gmail.com>
2020-11-28 14:20:46 +0100

    New approach

--- main.js
+++ main.js
@@ -1,74 +1,22 @@
 var doc = window.document,
-  context = doc.querySelector('.js-loop'),
-  clones = context.querySelectorAll('.is-clone'),
-  disableScroll = false,
-  scrollHeight = 0,
-  scrollPos = 0,
-  clonesHeight = 0,
-  i = 0;
+  context = doc.querySelector('.js-loop');
 
-function getScrollPos () {
-  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
-}
-
-function setScrollPos (pos) {
-  context.scrollTop = pos;
-}
-
-function getClonesHeight () {
-  clonesHeight = 0;
-
-  for (i = 0; i < clones.length; i += 1) {
-    clonesHeight = clonesHeight + clones[i].offsetHeight;
-  }
-
-  return clonesHeight;
-}
-
-function reCalc () {
-  scrollPos = getScrollPos();
-  scrollHeight = context.scrollHeight;
-  clonesHeight = getClonesHeight();
-
-  if (scrollPos <= 0) {
-    setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
-  }
-}
-
-function scrollUpdate () {
-  console.log(scrollPos + ' : ' + scrollHeight + ' : ' + context.clientHeight + ' : ' + context.scrollTop);
-  if (!disableScroll) {
-    scrollPos = getScrollPos();
-
-    if (clonesHeight + scrollPos >= scrollHeight/*scrollHeight - context.clientHeight === context.scrollTop*/) {
-      // Scroll to the top when you’ve reached the bottom
-      setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
-      disableScroll = true;
-    } else if (scrollPos <= 0) {
-      // Scroll to the bottom when you reach the top
-      setScrollPos(scrollHeight/* - clonesHeight*/);
-      disableScroll = true;
+function eightSections(n){
+    context.scrollTop = count;
+    count += 500;
+    n++
+    if(n<8) setTimeout(eightSections, 3000, n);
+    else{
+        n=0;
+        context.scrollTop = 0;
+        count = 0;
+        eightSections(0);
     }
-  }
-
-  if (disableScroll) {
-    // Disable scroll-jumping for a short time to avoid flickering
-    window.setTimeout(function () {
-      disableScroll = false;
-    }, 40);
-  }
+    console.log('eightSections(n) --> n: '+n)
 }
 
 function init () {
-  reCalc();
-  
-  context.addEventListener('scroll', function () {
-    window.requestAnimationFrame(scrollUpdate);
-  }, false);
-
-  window.addEventListener('resize', function () {
-    window.requestAnimationFrame(reCalc);
-  }, false);
+  eightSections(0);
 }
 
 if (document.readyState !== 'loading') {
@@ -76,8 +24,3 @@ if (document.readyState !== 'loading') {
 } else {
   doc.addEventListener('DOMContentLoaded', init, false)
 }
-
-// Just for this demo: Center the middle block on page load
-/*window.onload = function () {
-  setScrollPos(Math.round(clones[0].getBoundingClientRect().top + getScrollPos() - (context.offsetHeight - clones[0].offsetHeight) / 2));
-};*/
