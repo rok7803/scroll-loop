322b1e0b2e3f8e77eb4e5a53a42f5a42c415a367
ha6icht <rolf.karlen@gmail.com>
2020-11-28 15:27:29 +0100

    Insert onmouseenter and onmouseleave

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 var doc = window.document,
   context = doc.querySelector('.js-loop'),
-  count = 0;
+  count = 0,
+  n = 0,
+  myTimeout;
 
-function eightSections(n){
+function eightSections(){
     context.scrollTop = count;
     count += 500;
     n++;
-    if(n<8) setTimeout(eightSections, 1500, n);
+    if(n<8){
+        myTimeout = setTimeout(eightSections, 1500, n);
+    }
     else{
         n=0;
         context.scrollTop = 0;
         count = 0;
-        eightSections(0);
+        eightSections();
     }
     console.log('eightSections(n) --> n: '+n)
 }
 
-function init () {
-  eightSections(0);
+function stopTimeout(){
+    clearTimeout(myTimeout);
 }
+
+function init () {
+  eightSections();
+}
+
+context.onmouseenter = function() {stopTimeout()};
+context.onmouseleave = function() {eightSections()};
 
 if (document.readyState !== 'loading') {
   init()
